ef86a5afdf91955bb1d4ce5528fd962631012d34
Armyk <armycek@proton.me>
2026-04-03 23:38:20 +0200

    Vytvoření menu, vytvoření významu barev, smazání zbatečných mezer

--- linux.sh
+++ linux.sh
@@ -8,19 +8,43 @@ RED="\033[0;31m"
 GREEN="\033[0;32m"
 NC="\033[0m" # zakončení barvy
 BLUE='\033[0;34m'
+YELLOW='\033[1;33m'
+
+#==========================================================================================================================================
+#přidání menu pro program 
+
+clear
+echo -e "${BLUE}==============================${NC}"
+echo -e "${GREEN}       Vítej ve scriptu!       ${NC}"
+echo -e "${BLUE}==============================${NC}\n"
+
+echo -e "${YELLOW}O co jde:${NC}"
+echo -e "Tento script automaticky spustí úžitečné příkazy, které jsou potřeba při eskalaci privilegií."
+echo -e "${BLUE}Barevná logika (význam):${NC}"
+echo -e "${RED}Červená${NC} → HIGH / kritický"
+echo -e "${YELLOW}Žlutá${NC} → MEDIUM / podezřelé"
+echo -e "${GREEN}Zelená${NC} → LOW / info"
+echo -e "${BLUE}Modrá${NC} → INFO"
+# čekání na enter
+read -rp "Stiskni Enter pro pokračování..." 
+
+echo -e "${BLUE}Hlavní část scriptu se spouští...${NC}"
+
+sleep 1 #zastavení programu na 1 sekundu
+clear #příkaz pro clean reminálu
 
 #PRO ZÁKLADNÍ INFORMACE
 
 #==========================================================================================================================================
 
-echo -e "====${RED}ZÁKLÁDNÍ INFORMACE${NC}====\n"
+echo -e "====${BLUE}ZÁKLÁDNÍ INFORMACE${NC}====\n"
 
 USER=$(whoami)
 
 if [ "$USER" == "root" ]; then
-	echo "Jsi root"
+	echo -e "${RED}Jsi root${NC}"
 else
-	echo "Nejsi root"
+	echo -e "${GREEN}Nejsi root${NC}"
 	echo "Jsi uživatel: $USER"
 fi
 
@@ -30,21 +54,16 @@ groups #vypíše pouze group bez žádných id
 
 # -q u grepu pro quiet (grep nic nevypíše, jenom pro podmínku)
 if groups | grep -qw "docker"; then #-q pro quit a -w pro přesnou shodu celého zapsaného textu (docker)
-	echo -e "\n${GREEN}Nacházíš se v Docker group - využití eskalace priviligií!!${NC}\n"
+	echo -e "\n${RED}Nacházíš se v Docker group - využití eskalace priviligií!!${NC}\n"
 fi
 
 if [ "$EUID" -eq 0 ]; then #proměnná pro id uživatele
-	echo -e "\n${GREEN}Jsi root!!${NC}\n"
+	echo -e "\n${RED}Jsi root!!${NC}\n"
 fi
 
-
-
 #==========================================================================================================================================
-
-
 #SYSTEM INFO
-
-echo -e "\n====${RED}SYSTEM INFO${NC}====\n"
+echo -e "\n====${BLUE}SYSTEM INFO${NC}====\n"
 
 # ^ znamená začátek řádku, tak že to nebude vypisovat řádky, které obsahuje někde v textu name=
 grep "^NAME=" /etc/os-release
@@ -55,26 +74,20 @@ echo #Pro prázdný řádek
 
 #==========================================================================================================================================
 
-
-echo -e "\n====${RED}SUDO${NC}====\n"
-
+echo -e "\n====${BLUE}SUDO${NC}====\n"
 
 if sudo -n -l >/dev/null 2>&1; then # pokud je vyžadováno heslo, tak -n to když tak přeskočí
     echo "sudo -l jde využít bez hesla, ale pžíkazy vyžadují heslo"
 	if sudo -n -l 2>/dev/null | grep -q "NOPASSWD"; then   #vnoření podmínka
-		echo -e "${GREEN}EXISTUJÍ PŘÍKAZY BEZ HESLA${NC}"
+		echo -e "${YELLOW}EXISTUJÍ PŘÍKAZY BEZ HESLA${NC}"
 	fi
 else
     echo "sudo -l vyžaduje heslo nebo není dostupné"
 fi
 
-
-
-
 #==========================================================================================================================================
 
-
-echo -e "\n====${RED}SUID${NC}====\n"
+echo -e "\n====${BLUE}SUID${NC}====\n"
 
 #vytvoření systému hledání možných zranitelností v programech se SUID bitem
 
@@ -112,25 +125,19 @@ vagrant valgrind vi view vim vimdiff virsh volatility w3m watch wc wget whiptail
 wireshark wish xargs xdg-user-dir xdotool xelatex xetex xmodmap xmore xpad xxd xz yarn yash
 yelp yt-dlp zathura zcat zgrep zic zip zless zsh zsoelim zypper)
 
-
-
 find / -perm -4000 -type f 2>/dev/null | while IFS= read -r file; do   #-r aby nemazal v path /, IFS pro to, aby nerozděloval podle mezer a speciálních znaků
     binarka=$(basename "$file") # uložení do proměnné, basename znamená, že veme celou file (cestu souboru) a veme jenom poslední část, tak že pro nás název aplikace ezz prostě :D
 
     for item in "${seznam[@]}"; do #veme každý item z našeho velkého seznamu XDDD, zrovna mi tady pomohl ai jo :I, klid XD
         if [ "$binarka" = "$item" ]; then   #každý vypsaný řádek z find příkazu se projede touto hračkou a jestli se název programu ROVNÁ s nějakým z itemů v seznamu, tak to dá program vědět :D
-            echo -e "${RED}[!]UNPRIVILEGED SUID nalezen: $file${NC}"
+            echo -e "${YELLOW}[!]UNPRIVILEGED SUID nalezen: $file${NC}"
             echo "    → $binarka je v seznamu (zkontroluj GTFOBins)"
 	    echo -e "${BLUE}https://gtfobins.org/gtfobins/$item/${NC}"
         fi
     done
 done
 
-
 echo 
-
-
-
 
 find / -perm -4000 -type f 2>/dev/null #vypsání (jenom) souborů se suid bitem (složky to nevypisuje) 
 # tento příkaz find tu bude na určitou dobu, později bych projel možnosti v gtfobins a udělal podmínky na možnosti eskalace privilegií
