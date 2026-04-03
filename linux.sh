b94ced8999278b532dc6bd8349b7787c8c70a866
Armyk <armycek@proton.me>
2026-04-03 22:33:45 +0200

    Přidání / změna dalšího kódu

--- linux.sh
+++ linux.sh
@@ -78,3 +78,4 @@ echo -e "\n====${RED}SUID${NC}====\n"
 find / -perm -4000 -type f 2>/dev/null #vypsání (jenom) souborů se suid bitem (složky to nevypisuje) 
 # tento příkaz find tu bude na určitou dobu, později bych projel možnosti v gtfobins a udělal podmínky na možnosti eskalace privilegií
 
+find / -writable -type f 2>/dev/null | head -n 10
