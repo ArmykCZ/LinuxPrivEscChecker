8d91f0a911c75b1c2268e5dca02534301ee408d4
Armyk <armycek@proton.me>
2026-04-03 12:54:30 +0200

    Přidání vypsání základních informací a informace o systému

--- linux.sh
+++ linux.sh
@@ -8,7 +8,9 @@ RED="\033[0;31m"
 GREEN="\033[0;32m"
 NC="\033[0m" # zakončení barvy
 
-echo -e "====${RED}Základní informace${NC}===="
+#PRO ZÁKLADNÍ INFORMACE
+
+echo -e "====${RED}ZÁKLÁDNÍ INFORMACE${NC}====\n"
 
 USER=$(whoami)
 
@@ -16,6 +18,32 @@ if [ "$USER" == "root" ]; then
 	echo "Jsi root"
 else
 	echo "Nejsi root"
-	echo "Jsi uživatel" && whoami
+	echo "Jsi uživatel: " && whoami
 fi
+
+id
+
+# -q u grepu pro quiet (grep nic nevypíše, jenom pro podmínku)
+if id | grep -q "docker"; then
+	echo -e "${GREEN}Nacházíš se v Docker group - využití eskalace priviligií!!${NC}\n"
+fi
+
+if id | grep -q  "uid=0"; then
+	echo -e "${GREEN}Jsi root!!${NC}\n"
+fi
+
+
+
+#SYSTEM INFO
+
+echo -e "\n====${RED}SYSTEM INFO${NC}====\n"
+
+# ^ znamená začátek řádku, tak že to nebude vypisovat řádky, které obsahuje někde v textu name=
+cat /etc/os-release | grep "^NAME="
+echo #Pro prázdný řádek  
+uname -a 
+
+
+
+
 
